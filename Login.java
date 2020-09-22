71cd2c1e46efc678dabee2fcd42a3379438841a3
Richard David <48126051+itsarcanedave@users.noreply.github.com>
2020-09-22 17:36:58 +0700

    Bug fix on Splash.java

---
+++ Login.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Login extends JFrame implements ActionListener {
+    JPanel panel;
+    JLabel user_label, password_label, message;
+    JTextField userName_text;
+    JPasswordField password_text;
+    JButton submit, cancel;
+    
+    Login() {
+       
+      // Username Label
+       user_label = new JLabel();
+       user_label.setText("Usermame : sementara admin");
+       userName_text = new JTextField();
+       
+       // Password Label
+       password_label = new JLabel();
+       password_label.setText("Password : sementara admin");
+       password_text = new JPasswordField();
+       
+       // Submit
+       submit = new JButton("LOGIN");
+       panel = new JPanel(new GridLayout(3, 1));
+       panel.add(user_label);
+       panel.add(userName_text);
+       panel.add(password_label);
+       panel.add(password_text);
+       message = new JLabel();
+       panel.add(message);
+       panel.add(submit);
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+       // Adding listeners
+       submit.addActionListener(this);
+       add(panel, BorderLayout.CENTER);
+       setTitle("Project Lumen");
+       setSize(450,350);
+       setVisible(true);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+       String userName = userName_text.getText();
+       String password = password_text.getText();
+       //obsolete when database is ready
+       if (userName.trim().equals("admin") && password.trim().equals("admin")) { 
+          message.setText(" Hello " + userName + "");
+          new UI();
+       } else {
+          message.setText(" Invalid credentials! Please double-check your Username and Password.");
+       }
+    }
+ }
---
+++ Splash.java
@@ -0,0 +1,92 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+public class Splash extends JFrame implements ActionListener {
+    public static SplashScreen execute;
+    public static int count;
+ 
+    public static int delay = 5000;
+   
+    
+    
+        public Splash() {
+            super("Project Lumen");
+      
+            Container container = getContentPane();
+            container.setLayout(null);
+            
+            JPanel panel = new JPanel();
+            panel.setBorder(new javax.swing.border.EtchedBorder());
+            panel.setBackground(new Color(255, 255, 255));
+            panel.setBounds(10, 10, 348, 150);
+            panel.setLayout(null);
+            pack();
+            container.add(panel);
+    
+            JLabel label = new JLabel("Project Lumen");
+            label.setFont(new Font("Verdana", Font.BOLD, 14));
+            label.setBounds(85, 25, 280, 30);
+            panel.add(label);
+            
+            JProgressBar progressBar = new JProgressBar(0, 100);
+            progressBar.setStringPainted(true);
+            progressBar.setBounds(55, 180, 250, 15);
+            container.add(progressBar);
+            setSize(380, 275);
+            setLocationRelativeTo(null);
+            setVisible(true);
+            
+            Runnable schedulerRunnable = new Runnable() {
+                public void run() {
+                    int value = progressBar.getValue();
+                    progressBar.setValue(value + 1);
+                }
+            };
+            
+            ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+            executor.scheduleAtFixedRate(schedulerRunnable, 0, 50, TimeUnit.MILLISECONDS);
+            
+            
+            try {
+                
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            setVisible(false);
+            new Login();
+
+        
+     
+            
+           
+    
+        
+         
+           
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        
+
+        }
+
+
+
+           
+
+       
+        
+
+
+    
+    }
+    
